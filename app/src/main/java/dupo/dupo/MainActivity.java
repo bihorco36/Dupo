3c79878b4f3e3321939557366778cbaa742295d9
Janiss Binder <binder@puzzle.ch>
2016-03-22 12:58:05 +0100

    added some chnages

--- app/src/main/java/dupo/dupo/MainActivity.java
+++ app/src/main/java/dupo/dupo/MainActivity.java
@@ -1,13 +1,72 @@
 package dupo.dupo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    private MainActivity activity;
+
+    private List peers = new ArrayList();
+    WifiBroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+        WifiP2pDeviceList peerList = new WifiP2pDeviceList();
+        Collection test = peerList.getDeviceList();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WifiBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+            // Out with the old, in with the new.
+            peers.clear();
+            peers.addAll(peerList.getDeviceList());
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change.  For instance, if you have a ListView of available
+            // peers, trigger an update.
+            //((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+            if (peers.size() == 0) {
+                return;
+            }
+        }
+    };
 }
